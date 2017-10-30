c5ba3db3d11743ce71490e11bba8eb2fec35e666
Torres, Rafael (ESI) <Rafael.Torres@adp.com>
2017-10-30 19:37:39 -0200

    Adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+/**
+ * Created by rtorres on 25/10/2017.
+ */
+const path = require('path')
+const webpack = require('webpack');
+
+module.exports = {
+    //Application entrypoint
+    entry:{
+      app : "./app/app.module.js",
+      vendor: ["jquery", "angular", "chart.js"]
+    },
+
+    //Results of operations done by webpack
+    output : {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].bundle.js"
+    },
+    //Loaders used to deal with css, imgs, etc
+    module:{
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'] //Webpack execute loaders in reverse order: css should be the last one then.
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({}),
+        new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ]
+};
